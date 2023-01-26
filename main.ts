dd01ab55008b7bf6ada49b45d896d61972f9b4fb
imnoturbra <122376499+imnoturbra@users.noreply.github.com>
2023-01-26 11:49:32 -0500

    Mise à jour main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,73 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
-    game.over(false, effects.dissolve)
-})
+namespace SpriteKind {
+    export const goomba = SpriteKind.create()
+}
+function enemy_1 () {
+    for (let valeur of tiles.getTilesByType(assets.tile`myTile4`)) {
+        Goomba = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f e e e e f f . . . . 
+            . . . f f f f e e f f f f . . . 
+            . . . . f 1 f e e f 1 f . . . . 
+            . . . f e 1 1 f f 1 1 e f . . . 
+            . f f e e 1 f e e f 1 e e f f . 
+            f e e e e 1 f e e f 1 e e e e f 
+            f e e e e e e e e e e e e e e f 
+            f e e 1 f f f f f f f f 1 e e f 
+            . f e 1 e e e e e e e e 1 e f . 
+            . . f f f f f f f f f f f f . . 
+            . . . . f d d d d d d f . . . . 
+            . . . e e d d d d d d e e . . . 
+            . e e e e d d d d d d e e e e . 
+            e e e e e e d d d d e e e e e e 
+            e e e e e e . . . . e e e e e e 
+            `, SpriteKind.goomba)
+        tiles.placeOnTile(Goomba, valeur)
+        tiles.setTileAt(valeur, assets.tile`myTile6`)
+        if (Math.percentChance(50)) {
+            Goomba.vx = 50
+        } else {
+            Goomba.vx = -50
+        }
+    }
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Mario.vy == 0) {
         Mario.vy = -200
     }
 })
+let Goomba: Sprite = null
 let Mario: Sprite = null
 tiles.setCurrentTilemap(tilemap`niveau1`)
+enemy_1()
 Mario = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
+    . . . . . 2 2 2 2 2 . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 2 . . . 
+    . . . . e e e d d f d . . . . . 
+    . . . e d e d d d f d d d . . . 
+    . . . e d e e d d d f d d d . . 
+    . . . . e d d d d f f f f . . . 
+    . . . . . d d d d d d d . . . . 
+    . . . . 2 2 8 2 2 2 8 2 2 . . . 
+    . . . 2 2 2 8 2 2 2 8 2 2 2 . . 
+    . . 2 2 2 2 8 8 8 8 8 2 2 2 2 . 
+    . . d d . . . . . . . . . d d . 
+    . . d d d . . . . . . . d d d . 
+    . . d d . . . . . . . . . d d . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(Mario, 100, 0)
 Mario.x = 24
 Mario.y = 220
 Mario.ay = 400
 scene.cameraFollowSprite(Mario)
+game.onUpdate(function () {
+    for (let valeur of sprites.allOfKind(SpriteKind.goomba)) {
+        if (valeur.isHittingTile(CollisionDirection.Left)) {
+            Goomba.vx = 50
+        } else if (valeur.isHittingTile(CollisionDirection.Right)) {
+            Goomba.vx = -50
+        }
+    }
+})
